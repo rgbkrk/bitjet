491be50ae83aa364010979d85861f0db241fd2b0
Kyle Kelley <rgbkrk@gmail.com>
2015-02-18 17:04:57 -0800

    Bitdget here we go.

---
+++ bitdget.py
@@ -0,0 +1,10 @@
+from IPython.html.widgets import DOMWidget
+from IPython.utils.traitlets import Int, Unicode, List
+
+class BitWidget(DOMWidget):
+    _view_module = Unicode('nbextensions/bitdget', sync=True)
+    _view_name = Unicode('BitView', sync=True)
+
+
+    bitwidth = Int(2, sync=True)
+    data = List([True, False, True, False], sync=True)
