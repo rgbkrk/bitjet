e8db38159427c9f2c36def965f8f4fb5a84ac873
Kyle Kelley <rgbkrk@gmail.com>
2015-02-20 03:16:01 -0800

    Inherit from a base BitJetWidget class.

--- bitjet/bitjet.py
+++ bitjet/bitjet.py
@@ -17,39 +17,29 @@ def b64encode_json(a):
     '''
     return {'b64data': base64.b64encode(a)}
 
-class BitWidget(DOMWidget):
+class BitJetWidget(DOMWidget):
+    _view_module = Unicode('nbextensions/bitjet/bitjet', sync=True)
+
+    datawidth = Int(2, sync=True)
+
+    data = (
+            Bytes(sync=True, to_json=b64encode_json) |
+            Instance(ndarray, sync=True, to_json=b64encode_json)
+    )
+
+    blockwidth = Int(4, sync=True)
+    blockheight = Int(4, sync=True)
+
+class BitWidget(BitJetWidget):
     '''
     BitWidget provides a way to visualize a binary data stream by bits, so long as they
     come in as a bytes array or a numpy array.
     '''
-    _view_module = Unicode('nbextensions/bitjet/bitjet', sync=True)
     _view_name = Unicode('BitView', sync=True)
 
-    datawidth = Int(2, sync=True)
-
-    data = (
-            Bytes(sync=True, to_json=b64encode_json) |
-            Instance(ndarray, sync=True, to_json=b64encode_json)
-    )
-
-    blockwidth = Int(4, sync=True)
-    blockheight = Int(4, sync=True)
-
-class ByteWidget(DOMWidget):
+class ByteWidget(BitJetWidget):
     '''
     ByteWidget provides a way to visualize a binary data stream by bytes, so long as they
     come in as a bytes array or a numpy array.
     '''
-    _view_module = Unicode('nbextensions/bitjet/bitjet', sync=True)
     _view_name = Unicode('ByteView', sync=True)
-
-    datawidth = Int(2, sync=True)
-
-    data = (
-            Bytes(sync=True, to_json=b64encode_json) |
-            Instance(ndarray, sync=True, to_json=b64encode_json)
-    )
-
-    blockwidth = Int(4, sync=True)
-    blockheight = Int(4, sync=True)
-
