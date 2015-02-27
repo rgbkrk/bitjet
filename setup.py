53ae3f16a41bff31182c43c9df2847d7b792abf9
Jonathan Frederic <jdfreder@calpoly.edu>
2015-02-27 10:42:44 -0800

    Fix dependency bug.

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup
 try:
     from ipythonpip import cmdclass
 except:
-    cmdclass = lambda *args: None
+    import pip, importlib
+    pip.main(['install', 'ipython-pip']); cmdclass = importlib.import_module('ipythonpip').cmdclass
 
 setup(
     name='bitjet',
